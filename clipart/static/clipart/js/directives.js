4bc2d05aa39e43a84d0b08daf4e40d000990a267
Leon Vinneyto <vinneyto@gmail.com>
2014-08-16 00:31:55 +0400

    добавил возможность перетаскивания карти нок на холст

--- clipart/static/clipart/js/directives.js
+++ clipart/static/clipart/js/directives.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('clipart.directives', [])
+
+  //Директива для слайдера
+
   .directive('ngSlider', function() {
     return {
       restrict: 'A',
@@ -26,7 +29,10 @@ angular.module('clipart.directives', [])
       }
     }
   })
-  .directive('editor', function($window) {
+
+  //Директива для редактора включающая загрузку шаблона
+
+  .directive('editor', ['$rootScope', function($rootScope) {
     return {
       restrict: 'E',
       templateUrl: '/static/clipart/partials/editor.html',
@@ -47,17 +53,105 @@ angular.module('clipart.directives', [])
           element.find('.canvas-wrapper').css({'margin-top': -height / 2});
         });
 
-        // создаём прямоугольник
-        var rect = new fabric.Rect({
-          left: 100,
-          top: 100,
-          fill: 'red',
-          width: 50,
-          height: 50
+        //Вытаскиваем из события область перетаскиваемого объекта и извлекаем из ней путь к изображению
+        $rootScope.$on('drop', function(event, position) {
+          fabric.Image.fromURL(event.targetScope.image.src, function(img) {
+
+            img.setLeft(position.left - canvas.getWidth()/2);
+            img.setTop(position.top - canvas.getHeight()/2);
+
+            canvas.add(img);
+          });
+        });
+      }
+    }
+  }])
+
+  //Директива для галлерей с возможностю перетаскивать изображения
+
+  .directive('gallery', ['$http', '$log', function($http, $log) {
+    return {
+      restrict: 'E',
+      templateUrl: '/static/clipart/partials/gallery.html',
+      replace: true,
+      link: function(scope, element, attrs) {
+        //Загружаем список категорий
+        $http.get('/clipart/categories').success(function(data) {
+          scope.categories = data;
         });
 
-        // добавляем прямоугольник, чтобы он отобразился
-        canvas.add(rect);
+        //Калбэк для клика по чекбоксам категорий
+        scope.categoriesChanged = function(categories) {
+          var selectedCatIds = [];
+          categories.forEach(function(cat) {
+            if (cat.checked) {
+              selectedCatIds.push(cat.id);
+            }
+          });
+          $log.info(selectedCatIds);
+          if (selectedCatIds.length) {
+            //Грузим список картинок с данными категориями
+            $http({
+              url: '/clipart/images',
+              method: "GET",
+              params: { categories: selectedCatIds.join(',') }
+            }).success(function(data) {
+              $log.info(data);
+              scope.images = data;
+            });
+          } else {
+            scope.images = [];
+          }
+        };
+
+        //Таскалка
+      }
+    }
+  }])
+
+  //Директива для перетаскивания объектов dom
+
+  .directive('ngDraggable', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        element.draggable({
+          helper: 'clone',
+          appendTo: "body",
+          revert:'invalid',
+          containment: '#wrapper',
+          zIndex: 100
+        });
+
+        element.on('drop', function(event, position) {
+          scope.$emit('drop', position);
+        });
+
+        scope.$on('$destroy', function() {
+          element.draggable( "destroy" );
+        });
+      }
+    }
+  })
+
+  //Директива для бросания объектов
+
+  .directive('ngDroppable', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        element.droppable({
+          accept: '.image-wrapper',
+          drop: function(event, ui) {
+            var dr = ui.draggable;
+            dr.trigger('drop', ui.position);
+          }
+        });
+
+        scope.$on('$destroy', function() {
+          element.droppable( "destroy" );
+        });
+
       }
     }
   });
